6562a581aa12e3702a98401e9ed7929caa6fdb46
Denis Anchugov <denis.anchugov@futurice.com>
2016-09-15 15:47:29 +0300

    Remove endpoint mapping if endpoint id not found in SNS.

--- push_service/application.py
+++ push_service/application.py
@@ -208,6 +208,8 @@ def run_sns_command(command, *args, **kwargs):
         client_error = str(err)
         if "NotFound" in client_error:
             raise NotFoundException
+        if "No endpoint found" in err.response["Message"] and "InvalidParameter" in err.response["Code"]:
+            raise err 
         for error_re, error_code, error_message in BOTO_ERRORS:
             if error_re.match(client_error):
                 abort(error_code, error_message=error_message)
@@ -264,8 +266,8 @@ def remove_user_id_endpoint_id_mapping(endpoint_id, user_id):
         UpdateExpression="DELETE endpointIds :e",
         ExpressionAttributeValues={":e": {"SS": [endpoint_id]}}
     )
-
-    try:
+    
+try:
         dynamodb.delete_item(
             TableName = DYNAMODB_TABLE_NAME,
             Key = {"id": {"S": user_id}},
@@ -453,7 +455,10 @@ class PublishMessageToUser(Resource):
         user_id = decode_base64_id(user_id)
         message_ids = []
         for endpoint_id in retrieve_endpoint_ids_by_user_id(user_id):
-            message_ids.append(publish(data, endpoint_id))
+            try:
+                message_ids.append(publish(data, endpoint_id))
+            except Exception:
+                remove_user_id_endpoint_id_mapping(endpoint_id, user_id)
         return {"message_ids": message_ids}
 
 api.add_resource(Device, "/device")
